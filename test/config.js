dd58938fc22a4e2dce95a7558df440b38bf19a7e
sriosbluecore <79520714+sriosbluecore@users.noreply.github.com>
2023-02-16 14:45:50 -0500

    CI with github actions

--- test/config.js
+++ test/config.js
@@ -6,6 +6,17 @@ module.exports = ({
         username: 'admin@gmail.com',
         password: 'admin'
     },
+    CI: {
+        baseURL : 'http://localhost:8080/',
+        username: 'admin@gmail.com',
+        password: 'admin',
+        launchOptions: {
+            executablePath: process.env.PUPPETEER_EXEC_PATH,
+            headless: true,
+            args: ['--no-sandbox'],
+        },
+        timeout: 5000,
+    },
     test: {},
     prod: {}
 })[process.env.TESTENV || 'local'];
